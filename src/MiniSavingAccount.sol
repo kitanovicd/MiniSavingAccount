95eb61a14e6484f6e292f3844b1344ea5d7f8023
kitanovicd <kitanovic.danilo@outlook.com>
2023-06-24 23:23:19 +0200

    Finish basic unit tests

--- src/MiniSavingAccount.sol
+++ src/MiniSavingAccount.sol
@@ -4,38 +4,131 @@ pragma solidity ^0.8.13;
 import {Ownable} from "openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20} from "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {console} from "forge-std/console.sol";
 
 error AssetNotSupported();
+error RepayUnavailable();
+error LiquidationUnavailable();
 
 contract MiniSavingAccount is Ownable {
     using SafeERC20 for IERC20;
 
-    mapping(address => mapping(address => uint256)) public borrowRates;
-    mapping(address => mapping(address => uint256)) public healthFactors;
-
-    function configureAsset(
-        address lendingAsset,
-        address borrowingAsset,
-        uint256 borrowRate,
-        uint256 healthFactor
-    ) external onlyOwner {
-        borrowRates[lendingAsset][borrowingAsset] = borrowRate;
-        healthFactors[lendingAsset][borrowingAsset] = healthFactor;
+    struct BorrowInfo {
+        address borrowAsset;
+        address collateralAsset;
+        uint256 borrowAmount;
+        uint256 collateralAmount;
+        uint256 returnAmount;
+        uint256 returDateTimestamp;
     }
 
-    function configureAssetsBatched(
-        address[] calldata _lendingAssets,
-        address[] calldata _borrowingAssets,
-        uint256[] calldata _borrowRates,
-        uint256[] calldata _healthFactors
-    ) external onlyOwner {
-        for (uint256 i = 0; i < _lendingAssets.length; i++) {
-            borrowRates[_lendingAssets[i]][_borrowingAssets[i]] = _borrowRates[
-                i
-            ];
-            healthFactors[_lendingAssets[i]][
-                _borrowingAssets[i]
-            ] = _healthFactors[i];
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public lendingRatesDaily;
+    mapping(address => mapping(address => uint256)) public collateralRates;
+
+    BorrowInfo[] public borrowings;
+
+    function deposit(address asset, uint256 amount) external {
+        balances[asset] += amount;
+        IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(address asset, uint256 amount) external {
+        balances[asset] -= amount;
+        IERC20(asset).safeTransfer(msg.sender, amount);
+    }
+
+    function borrow(
+        address borrowAsset,
+        uint256 borrowAmount,
+        address collateralAsset,
+        uint256 period
+    ) external returns (uint256) {
+        uint256 collateralRate = collateralRates[borrowAsset][collateralAsset];
+
+        if (collateralRate == 0) {
+            revert AssetNotSupported();
         }
+
+        uint256 returnAmount = borrowAmount +
+            (lendingRatesDaily[borrowAsset] * period) /
+            1 ether;
+
+        uint256 collateralAmount = (returnAmount * collateralRate) / 1 ether;
+
+        borrowings.push(
+            BorrowInfo({
+                borrowAsset: borrowAsset,
+                collateralAsset: collateralAsset,
+                borrowAmount: borrowAmount,
+                collateralAmount: collateralAmount,
+                returnAmount: returnAmount,
+                returDateTimestamp: block.timestamp + period
+            })
+        );
+
+        balances[borrowAsset] -= borrowAmount;
+
+        IERC20(borrowAsset).safeTransfer(msg.sender, borrowAmount);
+        IERC20(collateralAsset).safeTransferFrom(
+            msg.sender,
+            address(this),
+            collateralAmount
+        );
+
+        return borrowings.length - 1;
+    }
+
+    function repay(uint256 index) external {
+        BorrowInfo storage borrowInfo = borrowings[index];
+
+        balances[borrowInfo.borrowAsset] += borrowInfo.returnAmount;
+
+        IERC20(borrowInfo.borrowAsset).safeTransferFrom(
+            msg.sender,
+            address(this),
+            borrowInfo.returnAmount
+        );
+        IERC20(borrowInfo.collateralAsset).safeTransfer(
+            msg.sender,
+            borrowInfo.collateralAmount
+        );
+
+        borrowInfo.collateralAmount = 0;
+        borrowInfo.returnAmount = 0;
+    }
+
+    function liquidate(uint256 index) external {
+        BorrowInfo storage borrowInfo = borrowings[index];
+
+        if (borrowInfo.returDateTimestamp >= block.timestamp) {
+            revert LiquidationUnavailable();
+        }
+
+        balances[borrowInfo.collateralAsset] += borrowInfo.collateralAmount;
+
+        borrowInfo.collateralAmount = 0;
+        borrowInfo.returnAmount = 0;
+    }
+
+    function setCollateralRate(
+        address lendingAsset,
+        address borrowingAsset,
+        uint256 borrowRate
+    ) external onlyOwner {
+        collateralRates[lendingAsset][borrowingAsset] = borrowRate;
+    }
+
+    function setDailyLendingRate(
+        address asset,
+        uint256 lendingRate
+    ) external onlyOwner {
+        lendingRatesDaily[asset] = lendingRate;
+    }
+
+    function getBorrowingInfo(
+        uint256 index
+    ) external view returns (BorrowInfo memory) {
+        return borrowings[index];
     }
 }
--- test/MiniSavingAccount.t.sol
+++ test/MiniSavingAccount.t.sol
@@ -2,7 +2,298 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {MintableERC20} from "./mock/MintableERC20.sol";
+import {MiniSavingAccount, AssetNotSupported, RepayUnavailable, LiquidationUnavailable} from "src/MiniSavingAccount.sol";
+import {console} from "forge-std/console.sol";
+import {Math} from "openzeppelin/contracts/utils/math/Math.sol";
 
 contract MiniSavingAccountTest is Test {
-    function setUp() public {}
+    address alice;
+    address bob;
+
+    MintableERC20 public usd;
+    MintableERC20 public eur;
+
+    MiniSavingAccount public account;
+
+    function setUp() public {
+        alice = makeAddr("Alice");
+        bob = makeAddr("Bob");
+
+        vm.deal(alice, 1000 ether);
+        vm.deal(bob, 1000 ether);
+
+        usd = new MintableERC20("United Stated Dollar", "USD");
+        eur = new MintableERC20("Euro", "EUR");
+
+        usd.mint(alice, 1000 ether);
+        usd.mint(bob, 1000 ether);
+        eur.mint(alice, 1000 ether);
+        eur.mint(bob, 1000 ether);
+
+        vm.startPrank(alice);
+        account = new MiniSavingAccount();
+        vm.stopPrank();
+    }
+
+    function testDeposit(uint256 amount) public {
+        amount = bound(amount, 0, usd.balanceOf(alice));
+
+        uint256 aliceBalanceBefore = usd.balanceOf(alice);
+
+        vm.startPrank(alice);
+        usd.approve(address(account), amount);
+        account.deposit(address(usd), amount);
+        vm.stopPrank();
+
+        uint256 aliceBalanceAfter = usd.balanceOf(alice);
+
+        assertEq(account.balances(address(usd)), amount);
+        assertEq(usd.balanceOf(address(account)), amount);
+        assertEq(aliceBalanceBefore - aliceBalanceAfter, amount);
+    }
+
+    function testWithdraw(
+        uint256 depositAmount,
+        uint256 withdrawAmount
+    ) public {
+        depositAmount = bound(depositAmount, 0, usd.balanceOf(alice));
+        withdrawAmount = bound(withdrawAmount, 0, depositAmount);
+
+        vm.startPrank(alice);
+        usd.approve(address(account), depositAmount);
+        account.deposit(address(usd), depositAmount);
+        vm.stopPrank();
+
+        uint256 aliceBalanceBefore = usd.balanceOf(alice);
+
+        vm.startPrank(alice);
+        account.withdraw(address(usd), withdrawAmount);
+        vm.stopPrank();
+
+        uint256 aliceBalanceAfter = usd.balanceOf(alice);
+
+        assertEq(
+            account.balances(address(usd)),
+            depositAmount - withdrawAmount
+        );
+        assertEq(
+            usd.balanceOf(address(account)),
+            depositAmount - withdrawAmount
+        );
+        assertEq(aliceBalanceAfter - aliceBalanceBefore, withdrawAmount);
+    }
+
+    function testSetCollateralRate(uint256 amount) public {
+        vm.startPrank(alice);
+        account.setCollateralRate(address(usd), address(eur), amount);
+        vm.stopPrank();
+
+        assertEq(account.collateralRates(address(usd), address(eur)), amount);
+    }
+
+    function testSetDailyLendingRate(uint256 amount) public {
+        vm.startPrank(alice);
+        account.setDailyLendingRate(address(usd), amount);
+        vm.stopPrank();
+
+        assertEq(account.lendingRatesDaily(address(usd)), amount);
+    }
+
+    function testBorrow() public {
+        uint256 borrowingPeriod = 365 days;
+        uint256 depositAmount = 3 ether;
+        uint256 borrowAmount = 1 ether;
+        uint256 lendingRate = 1 ether / 100;
+        uint256 collateralRate = (16 * 1 ether) / 10;
+
+        vm.startPrank(alice);
+        account.setCollateralRate(address(usd), address(eur), collateralRate);
+        account.setDailyLendingRate(address(usd), lendingRate);
+
+        usd.approve(address(account), depositAmount);
+        account.deposit(address(usd), depositAmount);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+
+        uint256 bobUsdBalanceBefore = usd.balanceOf(bob);
+        uint256 bobEurBalanceBefore = eur.balanceOf(bob);
+
+        uint256 returnAmount = borrowAmount +
+            (lendingRate * borrowingPeriod) /
+            1 ether;
+        uint256 collateralAmount = (returnAmount * collateralRate) / 1 ether;
+
+        eur.approve(address(account), collateralAmount);
+        uint256 borrowIndex = account.borrow(
+            address(usd),
+            borrowAmount,
+            address(eur),
+            borrowingPeriod
+        );
+
+        vm.stopPrank();
+
+        uint256 bobUsdBalanceAfter = usd.balanceOf(bob);
+        uint256 bobEurBalanceAfter = eur.balanceOf(bob);
+
+        assertEq(borrowIndex, 0);
+        assertEq(account.balances(address(usd)), depositAmount - borrowAmount);
+        assertEq(account.balances(address(eur)), 0);
+        assertEq(eur.balanceOf(address(account)), collateralAmount);
+        assertEq(usd.balanceOf(address(account)), depositAmount - borrowAmount);
+        assertEq(bobUsdBalanceAfter - bobUsdBalanceBefore, borrowAmount);
+        assertEq(bobEurBalanceBefore - bobEurBalanceAfter, collateralAmount);
+
+        MiniSavingAccount.BorrowInfo memory borrowInfo = account
+            .getBorrowingInfo(0);
+
+        assertEq(borrowInfo.borrowAsset, address(usd));
+        assertEq(borrowInfo.collateralAsset, address(eur));
+        assertEq(borrowInfo.borrowAmount, borrowAmount);
+        assertEq(borrowInfo.collateralAmount, collateralAmount);
+        assertEq(borrowInfo.returnAmount, returnAmount);
+        assertEq(
+            borrowInfo.returDateTimestamp,
+            block.timestamp + borrowingPeriod
+        );
+    }
+
+    function testRepay() public {
+        uint256 borrowingPeriod = 365 days;
+        uint256 depositAmount = 3 ether;
+        uint256 borrowAmount = 1 ether;
+        uint256 lendingRate = 1 ether / 100;
+        uint256 collateralRate = (16 * 1 ether) / 10;
+
+        vm.startPrank(alice);
+
+        account.setCollateralRate(address(usd), address(eur), collateralRate);
+        account.setDailyLendingRate(address(usd), lendingRate);
+        usd.approve(address(account), depositAmount);
+        account.deposit(address(usd), depositAmount);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 returnAmount = borrowAmount +
+            (lendingRate * borrowingPeriod) /
+            1 ether;
+        uint256 collateralAmount = (returnAmount * collateralRate) / 1 ether;
+
+        eur.approve(address(account), collateralAmount);
+        account.borrow(
+            address(usd),
+            borrowAmount,
+            address(eur),
+            borrowingPeriod
+        );
+
+        usd.approve(address(account), returnAmount);
+        account.repay(0);
+
+        vm.stopPrank();
+
+        assertEq(
+            account.balances(address(usd)),
+            depositAmount - borrowAmount + returnAmount
+        );
+        assertEq(account.balances(address(eur)), 0);
+        assertEq(eur.balanceOf(address(account)), 0);
+        assertEq(
+            usd.balanceOf(address(account)),
+            depositAmount - borrowAmount + returnAmount
+        );
+
+        MiniSavingAccount.BorrowInfo memory borrowInfo = account
+            .getBorrowingInfo(0);
+
+        assertEq(borrowInfo.collateralAmount, 0);
+        assertEq(borrowInfo.returnAmount, 0);
+    }
+
+    function testLiquidate() public {
+        uint256 borrowingPeriod = 365 days;
+        uint256 depositAmount = 3 ether;
+        uint256 borrowAmount = 1 ether;
+        uint256 lendingRate = 1 ether / 100;
+        uint256 collateralRate = (16 * 1 ether) / 10;
+
+        vm.startPrank(alice);
+
+        account.setCollateralRate(address(usd), address(eur), collateralRate);
+        account.setDailyLendingRate(address(usd), lendingRate);
+        usd.approve(address(account), depositAmount);
+        account.deposit(address(usd), depositAmount);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 returnAmount = borrowAmount +
+            (lendingRate * borrowingPeriod) /
+            1 ether;
+        uint256 collateralAmount = (returnAmount * collateralRate) / 1 ether;
+
+        eur.approve(address(account), collateralAmount);
+        account.borrow(
+            address(usd),
+            borrowAmount,
+            address(eur),
+            borrowingPeriod
+        );
+
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + borrowingPeriod + 1);
+        account.liquidate(0);
+
+        assertEq(account.balances(address(usd)), depositAmount - borrowAmount);
+        assertEq(account.balances(address(eur)), collateralAmount);
+        assertEq(usd.balanceOf(address(account)), depositAmount - borrowAmount);
+        assertEq(eur.balanceOf(address(account)), collateralAmount);
+
+        MiniSavingAccount.BorrowInfo memory borrowInfo = account
+            .getBorrowingInfo(0);
+
+        assertEq(borrowInfo.collateralAmount, 0);
+        assertEq(borrowInfo.returnAmount, 0);
+    }
+
+    function testLiquidateRevertLiquidationUnavailable() public {
+        uint256 borrowingPeriod = 365 days;
+        uint256 depositAmount = 3 ether;
+        uint256 borrowAmount = 1 ether;
+        uint256 lendingRate = 1 ether / 100;
+        uint256 collateralRate = (16 * 1 ether) / 10;
+
+        vm.startPrank(alice);
+
+        account.setCollateralRate(address(usd), address(eur), collateralRate);
+        account.setDailyLendingRate(address(usd), lendingRate);
+        usd.approve(address(account), depositAmount);
+        account.deposit(address(usd), depositAmount);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 returnAmount = borrowAmount +
+            (lendingRate * borrowingPeriod) /
+            1 ether;
+        uint256 collateralAmount = (returnAmount * collateralRate) / 1 ether;
+
+        eur.approve(address(account), collateralAmount);
+        account.borrow(
+            address(usd),
+            borrowAmount,
+            address(eur),
+            borrowingPeriod
+        );
+
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + borrowingPeriod - 1);
+        vm.expectRevert(LiquidationUnavailable.selector);
+        account.liquidate(0);
+    }
 }
